a9066516591d9c065339dd09fb8bed4ed1ae3f0c
peck liu <xiaoyang20170204@gmail.com>
2017-11-12 20:23:00 +0800

    feature：use namespace and rebind event

---
+++ chapter3/040.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $('#switcher').click(function(event) {
+    if ($(event.target).is('button')) {
+      var bodyClass = event.target.id.split('-')[1];
+      $('body').removeClass().addClass(bodyClass);
+      $('#switcher button').removeClass('selected');
+      $(event.target).addClass('selected');
+      event.stopPropagation();
+    } else {
+      $('#switcher button').toggleClass('hidden');
+    }
+  });
+  
+  $('#switcher').on('click.collapse', function(event) {
+    if (!$(event.target).is('button')) {
+      $('#switcher button').toggleClass('hidden');
+    }
+  });
+  $('#switcher-narrow, #switcher-large').click(function() {
+    $('#switcher').off('click.collapse');
+  });
+});
